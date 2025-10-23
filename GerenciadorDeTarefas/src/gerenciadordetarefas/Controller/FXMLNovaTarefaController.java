e9b2ef2740f3ebab8c563224783e3b4dc2fa1d97
William Rafael Fagundes <williamrafaelfagundes02@gmail.com>
2025-10-23 01:21:58 -0300

    Criado Design da tela NovaTarefa

---
+++ GerenciadorDeTarefas/src/gerenciadordetarefas/Controller/FXMLNovaTarefaController.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package gerenciadordetarefas.Controller;
+
+import java.awt.Button;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author willi
+ */
+public class FXMLNovaTarefaController implements Initializable {
+    
+    
+    @FXML
+    private TextArea inputDescricaoTarefa;
+
+    @FXML
+    private Button btnAdicionarTarefa;
+
+    @FXML
+    private TextField inputTitulo;
+
+    @FXML
+    private ComboBox<?> comboboxStatus;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
